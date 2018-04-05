0a3b6ae6f642f64f9e6d4a0a9678fba97d45607a
Hainer Savimaa <hainer.savimaa@aasaglobal.com>
2018-04-05 16:17:57 +0300

    FRONTENT Landing form changed input to select

--- public/src/scripts/functions.js
+++ public/src/scripts/functions.js
@@ -8,38 +8,18 @@ $(document).ready(function(){
     $loanAmountSlider.rangeslider({
         polyfill: false
       }).on('input', function() {
-        if (this.value == 0) {
-            $loanAmount.val("0 PLN");
-        } else if (this.value == '') {
-          $loanAmount.val("0 PLN");
-        }
-        else {
-            $loanAmount.val(this.value +" PLN");
-        }
+          $loanAmount.val(this.value);
       });
-    $loanAmount.on('input', function() {
-      if (this.value === "") {
-            this.value = 0;
-        }
+    $loanAmount.on('change', function() {
       $loanAmountSlider.val(this.value).change();
     });
 
     $loanDurationSlider.rangeslider({
         polyfill: false
       }).on('input', function() {
-        if (this.value == 0) {
-            $loanDuration.val("0");
-        } else if (this.value == '') {
-          $loanDuration.val("0");
-        }
-        else {
-            $loanDuration.val(this.value);
-        }
+          $loanDuration.val(this.value);
       });
-    $loanDuration.on('input', function() {
-      if (this.value === "") {
-            this.value = 0;
-        }
+    $loanDuration.on('change', function() {
       $loanDurationSlider.val(this.value).change();
     });
   });
