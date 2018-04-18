3458e6f09e7eb745733c32a0fc393d82936c3bb5
hendrik <hendrik.uibopuu@aasaglobal.com>
2018-04-18 15:20:51 +0300

    Prepare e-mail delivery

---
+++ tests/Unit/Base/Components/EmailDeliveryTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hendrik
+ * Date: 18.04.18
+ * Time: 13:50
+ */
+
+namespace Tests\Unit\Base\Components;
+
+use App\Base\Components\EmailDelivery;
+use PHPUnit\Framework\TestCase;
+
+class EmailDeliveryTest extends TestCase
+{
+
+}
