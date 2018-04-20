b84680ccb6f961ccd39b2a23eda5ac789dbecfc5
hendrik <hendrik.uibopuu@aasaglobal.com>
2018-04-20 15:06:17 +0300

    Change JWT leeway

--- src/Base/Components/GoogleAuthenticator.php
+++ src/Base/Components/GoogleAuthenticator.php
@@ -56,7 +56,7 @@ class GoogleAuthenticator implements AuthenticationServiceInterface
    */
   public function __construct()
   {
-    JWT::$leeway = 1000;
+    JWT::$leeway = 10;
     $this->client = new \Google_Client(['client_id' => self::CLIENT_ID]);
   }
 
