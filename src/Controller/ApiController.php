01d804f3c4c23b062c4d70a27a0dc967a2ca3142
hendrik <hendrik.uibopuu@aasaglobal.com>
2018-05-08 10:59:21 +0300

    Start work on ApiController

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hendrik
+ * Date: 08.05.18
+ * Time: 10:01
+ */
+
+namespace App\Controller;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class ApiController
+{
+  public function updateAction(Request $request, Response $response, $args)
+  {
+    
+  }
+}
