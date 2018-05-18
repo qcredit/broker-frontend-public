aa0ff3c5c59075526c98925906ea577f433d2bfd
hendrik <hendrik.uibopuu@aasaglobal.com>
2018-05-18 15:29:36 +0300

    Some changes

--- src/cron.php
+++ src/cron.php
@@ -17,6 +17,7 @@ $jobby->add('SendChooseOfferReminder', [
     require(__DIR__ . '/../vendor/autoload.php');
     $settings = require(__DIR__ . '/settings.php');
     $app = new \Slim\App($settings);
+    var_dump(getenv('ENV_TYPE'));
     $container = $app->getContainer();
     require(__DIR__ . '/dependencies.php');
     $job = new App\Cron\SendChooseOfferReminder(
