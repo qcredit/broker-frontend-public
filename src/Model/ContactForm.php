2267cfa484f55db90b0e3bc54028cb509b6df32f
hendrik <hendrik.uibopuu@aasaglobal.com>
2018-05-24 12:04:07 +0300

    Add recaptcha server-side

--- src/Model/ContactForm.php
+++ src/Model/ContactForm.php
@@ -21,6 +21,8 @@ class ContactForm
   const ATTR_NAME = 'name';
   const ATTR_EMAIL = 'email';
   const ATTR_MESSAGE = 'message';
+  const ATTR_RECAPTCHA = 'g-recaptcha-response';
+
   /**
    * @var Contact
    */
