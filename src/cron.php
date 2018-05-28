269e3d4e982d2bdaeb2f0192f5abeb8ac3866a60
hendrik <hendrik.uibopuu@aasaglobal.com>
2018-05-28 17:24:30 +0300

    Introduce Redlock for cronjobs

--- src/cron.php
+++ src/cron.php
@@ -1,32 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hendrik
- * Date: 24.04.18
- * Time: 10:24
- */
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use malkusch\lock\mutex\PHPRedisMutex;
+
+$redis = new Redis();
+$redis->connect('redis:6379');
 
 $jobby = new Jobby\Jobby([
   'debug' => false,
   'output' => '/var/log/apache2/broker-cron.log'
 ]);
 
-$jobby->add('SendChooseOfferReminder', [
-  'closure' => function() {
-    require(__DIR__ . '/../vendor/autoload.php');
-    $settings = require(__DIR__ . '/settings.php');
-    $app = new \Slim\App($settings);
-    require(__DIR__ . '/dependencies.php');
-    $job = new App\Cron\SendChooseOfferReminder(
-      $container,
-      $container->get('MessageDeliveryService'),
-      $container->get('ApplicationRepository'),
-      $container->get('MessageTemplateRepository')
-    );
-    return $job->run();
-  },
-  'schedule' => '*/2 * * * *',
-]);
+$mutex = new PHPRedisMutex([$redis], 'SendChooseOfferReminder');
+
+$mutex->check(function() use ($jobby) {
+  return true;
+})->then(function() use ($jobby) {
+
+  $jobby->add('SendChooseOfferReminder', [
+    'closure' => function() {
+      require(__DIR__ . '/../vendor/autoload.php');
+      $settings = require(__DIR__ . '/settings.php');
+      $app = new \Slim\App($settings);
+      require(__DIR__ . '/dependencies.php');
+      $job = new App\Cron\SendChooseOfferReminder(
+        $container,
+        $container->get('MessageDeliveryService'),
+        $container->get('ApplicationRepository'),
+        $container->get('MessageTemplateRepository')
+      );
+      return $job->run();
+    },
+    'schedule' => '*/2 * * * *',
+  ]);
+
+  sleep(10);
+
+});
 
 $jobby->run();
